f4ec46e45c58ce32fddedaf11fb200fa027f7007
Irvin Flores <irvinf413@gmail.com>
2021-04-17 13:31:33 -0700

    Here we go again
    Here we go again

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 import os
+
+#Secret key is generated to be 32 randomized string characters
 SECRET_KEY = os.urandom(32)
 
+#secure session form with csrg protections
+#csrf is a type of browser attack
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    # member(field that is diplayed in your site)
+    name = StringField('Enter Your Name', validators=[DataRequired()])
+    file_upload = FileField('Upload Your File', validators=[FileRequired()])
 
 app = Flask(__name__)
 
@@ -24,3 +31,6 @@ def home():
         description="Smarter page templates with Flask & Jinja.",
         form=form
     )
+    
+if __name__ == '__main__':
+    app.run()
