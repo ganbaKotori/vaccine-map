397d5910cda280aff901471559f378b594cc2444
David <garzadavid26@yahoo.com>
2021-04-17 16:50:32 -0700

    photo submission
    Trying to figure out and integrate the pic uploader

--- app.py
+++ app.py
@@ -10,22 +10,24 @@ from flask import Flask, render_template, jsonify, redirect
 
 import os
 SECRET_KEY = os.urandom(32)
+UPLOAD_FOLDER = 'vaccine-map\photo-submission'
 
 app = Flask(__name__)
 
-client = MongoClient('', ssl=True,ssl_cert_reqs='CERT_NONE')
+client = MongoClient('mongodb+srv://aramirez:Alex123@cluster0.bii1t.mongodb.net/smartcitizen?retryWrites=true&w=majority', ssl=True,ssl_cert_reqs='CERT_NONE')
 
 mydb = client["smartcitizen"]
 mycol = mydb["zipCodes"]
 
-myquery = {"properties.GEOID10": "90040"}
+myquery = {"properties.GEOID10": "90723"}
 
 mydoc = mycol.find(myquery)
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('Name:', validators=[DataRequired()])
 
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
